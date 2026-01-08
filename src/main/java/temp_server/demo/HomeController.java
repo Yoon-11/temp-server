635c5f1cafee6f25bf400f433e96d37aa123de71
Yoon-11 <jiys5799@gmail.com>
2026-01-08 16:47:21 +0900

    Add return Http OK

--- src/main/java/temp_server/demo/HomeController.java
+++ src/main/java/temp_server/demo/HomeController.java
@@ -1,6 +1,8 @@
 package temp_server.demo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Come On~!";
+    }
+
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck() {
     }
 }
